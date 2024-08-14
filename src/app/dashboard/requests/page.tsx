ae1dfce43c944f1c4a8b7b16ed0323a10e0427f0
marianWadia <marianwadia55@gmail.com>
2024-08-14 14:24:20 +0300

    started page friend requests

---
+++ src/app/dashboard/requests/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface RequestsPage {
+  
+}
+
+const RequestsPage: FC<RequestsPage> = ({}) => {
+  return <div>RequestsPage</div>
+}
+
+export default RequestsPage
